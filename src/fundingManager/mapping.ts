339b69ce1dd63074b4b10ad506032e892c07b1dd
Fabian Scherer <fabiansche@gmail.com>
2024-07-09 23:44:31 +0200

    feat: indexes buy txs as Swaps

--- src/fundingManager/mapping.ts
+++ src/fundingManager/mapping.ts
@@ -1,17 +1,21 @@
 import { FM_BC_Restricted_Bancor_Redeeming_VirtualSupply_v1 } from 'generated';
-import { initBondingCurve } from './initializers';
-import { getModuleTitleFromId } from '../utils';
+import { bondingCurve, swap } from './schema';
+import { SwapType_t } from 'generated/src/db/Enums.gen';
+import { Swap_t } from 'generated/src/db/Entities.gen';
 
 FM_BC_Restricted_Bancor_Redeeming_VirtualSupply_v1.ModuleInitialized.handler(
   async ({ event, context }) => {
-    const moduleTitle = getModuleTitleFromId(
-      event.params.moduleTitle as `0x${string}`
-    );
-    const newBc = initBondingCurve({
-      id: event.srcAddress,
-      name: moduleTitle!,
+    const mandatoryParams = {
+      bcType:
+        event.params.moduleTitle ===
+        'FM_BC_Restricted_Bancor_Redeeming_VirtualSupply_v1'
+          ? 'RESTRICTED'
+          : 'OPEN',
       orchestrator: event.params.parentOrchestrator,
-    });
+      id: event.srcAddress,
+    };
+    const newBc = { ...bondingCurve, ...mandatoryParams };
+
     context.BondingCurve.set(newBc);
   }
 );
@@ -82,7 +86,7 @@ FM_BC_Restricted_Bancor_Redeeming_VirtualSupply_v1.IssuanceTokenUpdated.handler(
   }
 );
 
-// Issuance Token
+// Collateral Token
 
 FM_BC_Restricted_Bancor_Redeeming_VirtualSupply_v1.CollateralTokenSet.handler(
   async ({ event, context }) => {
@@ -93,5 +97,25 @@ FM_BC_Restricted_Bancor_Redeeming_VirtualSupply_v1.CollateralTokenSet.handler(
       ...currentEntity!,
       collateralToken: event.params.token,
     });
+  }
+);
+
+/*
+  SWAPS
+*/
+
+// buy
+FM_BC_Restricted_Bancor_Redeeming_VirtualSupply_v1.TokensBought.handler(
+  async ({ event, context }) => {
+    const mandatoryParams = {
+      id: event.transactionIndex + '-' + event.transactionHash,
+      swapType: 'BUY',
+    };
+    const newBuy = {
+      ...swap,
+      ...mandatoryParams,
+    } as unknown as Swap_t;
+
+    context.Swap.set(newBuy);
   }
 );
---
+++ src/fundingManager/schema.ts
@@ -0,0 +1,18 @@
+export const bondingCurve = {
+  id: undefined,
+  bcType: undefined,
+  orchestrator: undefined,
+  buyFee: undefined,
+  sellFee: undefined,
+  virtualCollateral: undefined,
+  virtualIssuance: undefined,
+  buyReserveRatio: undefined,
+  sellReserveRatio: undefined,
+  issuanceToken: undefined,
+  collateralToken: undefined,
+};
+
+export const swap = {
+  id: undefined,
+  swapType: undefined,
+};
